ad6592b0c11829822fc88e9a17634eda6e5c4710
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-09-27 18:23:26 +0300

    Make unsigned integer appending method a free function

--- include/socks5/request.hpp
+++ include/socks5/request.hpp
@@ -1,6 +1,8 @@
 #ifndef LIBSOCKS5_REQUEST_HPP
 #define LIBSOCKS5_REQUEST_HPP
 
+#include "socks5/detail/type_traits/iterator.hpp"
+
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
@@ -12,25 +14,36 @@
 
 namespace socks5 {
 
+namespace detail {
+
+template <typename T,
+          std::size_t size = sizeof(T),
+          typename         = std::enable_if_t<std::is_unsigned_v<T>>>
+inline void append_uint(T num, std::vector<std::uint8_t> &buf) noexcept {
+    static_assert(size >= 1 && size <= 8 && size <= sizeof(T));
+
+    buf.emplace_back((num >> (8 * (size - 1))) & 0xFF);
+    if constexpr (size - 1 > 0) {
+        append_uint<T, size - 1>(num, buf);
+    }
+}
+
+} // namespace detail
+
 struct request final {
     using container_type = std::vector<std::uint8_t>;
     using const_iterator = container_type::const_iterator;
 
     request() = default;
 
-    template <typename T,
-              std::size_t size = sizeof(T),
-              typename         = std::enable_if_t<std::is_unsigned_v<T>>>
+    template <typename T, typename = std::enable_if_t<std::is_unsigned_v<T>>>
     inline void put(T num) noexcept {
-        static_assert(size >= 1 && size <= 8 && size <= sizeof(T));
-
-        buf_.push_back((num >> (8 * (size - 1))) & 0xFF);
-        if constexpr (size - 1 > 0) {
-            put<T, size - 1>(num, buf_);
-        }
+        socks5::detail::append_uint(num, buf_);
     }
 
-    template <typename Iterator>
+    template <typename Iterator,
+              typename = std::enable_if_t<
+                  socks5::detail::type_traits::is_iterator_v<Iterator>>>
     inline void put(const Iterator &begin, const Iterator &end) {
         std::copy(begin, end, std::back_inserter(buf_));
     }
