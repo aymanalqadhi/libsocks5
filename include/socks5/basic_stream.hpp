baeee7c1dbee1ce6f64d35fed6ed082381f45332
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-10-01 17:32:32 +0300

    Implement basic socks5 stream methods

--- include/socks5/basic_stream.hpp
+++ include/socks5/basic_stream.hpp
@@ -1,10 +1,10 @@
 #ifndef LIBSOCKS5_BASIC_STREAM_HPP
 #define LIBSOCKS5_BASIC_STREAM_HPP
 
-#include <boost/asio/ip/tcp.hpp>
+#include "socks5/detail/async/util.hpp"
 
-#include <cstdint>
-#include <string_view>
+#include <boost/asio/ip/address.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 namespace socks5 {
 
@@ -12,17 +12,22 @@ template <typename AsyncStream>
 struct basic_stream {
     using executor_type = typename AsyncStream::executor_type;
 
-    basic_stream(boost::asio::io_context &io) : io_ {io} {
+    basic_stream(boost::asio::io_context &io) : io_ {io}, sock_ {io} {
     }
 
     template <typename CompletionToken>
-    decltype(auto) async_socks5_open(std::string_view host,
-                                     std::uint16_t    port = 1080) {
-        // TODO: Implement
+    decltype(auto) async_socks5_open(boost::asio::ip::tcp::endpoint ep,
+                                     CompletionToken &&             token) {
+        return sock_.async_connect(std::move(ep),
+                                   std::forward<CompletionToken>(token));
     }
 
     inline void socks5_close() {
-        // TODO: Implement
+        sock_.close();
+    }
+
+    inline auto is_socks5_open() const noexcept -> bool {
+        return sock_.is_open();
     }
 
     inline auto get_executor() -> executor_type {
