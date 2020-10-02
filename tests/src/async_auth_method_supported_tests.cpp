2633c4c243c5cde0cadaa31b7a9eea983af94892
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-10-02 17:08:25 +0300

    Test the asynchronous socks5 authentication method checking operation

---
+++ tests/src/async_auth_method_supported_tests.cpp
@@ -0,0 +1,55 @@
+#include "socks5/detail/async/auth_method_supported.hpp"
+#include "socks5/error_code.hpp"
+#include "socks5/tests/fixtures/socket.hpp"
+#include "socks5/tests/util/random.hpp"
+
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/write.hpp>
+#include <gtest/gtest.h>
+
+#include <array>
+#include <cstdint>
+#include <string_view>
+
+#include <iostream>
+
+#include <version>
+namespace socks5::tests {
+
+struct AsyncAuthMethodSupportedTest
+    : socks5::tests::fixtures::SocketTest<true> {};
+
+TEST_F(AsyncAuthMethodSupportedTest, NormalValuesTest) {
+    std::array<std::uint8_t, 2> respbuf {0x05, 0xFF};
+
+    auto rnd = []() -> std::uint8_t {
+        return socks5::tests::util::random<std::uint8_t>();
+    };
+
+    auto r1 = rnd(), r2 = rnd(), r3 = rnd(), r4 = rnd(), chosen = rnd();
+
+    boost::asio::write(socket.input_pipe(), boost::asio::buffer(respbuf));
+    socks5::detail::async::async_auth_method_supported(
+        socket,
+        [](const auto &err, auto selected) {
+            EXPECT_TRUE(err);
+            EXPECT_EQ(err.value(),
+                      static_cast<int>(
+                          socks5::error_code::auth_method_not_supported));
+        },
+        r1, r2, r3, r4, chosen);
+    io.run();
+
+    respbuf[1] = chosen;
+    boost::asio::write(socket.input_pipe(), boost::asio::buffer(respbuf));
+    socks5::detail::async::async_auth_method_supported(
+        socket,
+        [](const auto &err, auto selected) {
+            EXPECT_FALSE(err);
+        },
+        r1, r2, r3, r4, chosen);
+    io.run();
+}
+
+} // namespace socks5::tests
